5814783ea6ab82963d9c2bcbdf799f279b589079
AshakaE <ashakaegerega@gmail.com>
2022-01-02 21:00:22 +0100

    Add User model validations

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,33 @@ const User = require('../models/User')
 const asyncHandler = require('../utils/asyncHandler')
 
 exports.signUp = asyncHandler(async (req, res, next) => {
-    const { name, email } = req.body
-    const user = await User.create({ name, email })
-    res.status(200).json({
-        data: user,
-    })
+    const { name, email, password, passwordConfirm } = req.body
+    try {
+        const user = await User.create({
+            name,
+            email,
+            password,
+            passwordConfirm,
+        })
+        res.status(200).json({
+            data: user,
+        })
+    } catch (e) {
+        res.send(e.errors[0].message)
+    }
     next()
 })
+
+// exports.login = asyncHandler(async (req, res, next) => {
+//     const { email, password } = req.body
+
+//     if (!email || !password) {
+//         return next(new AppError('Please provide email and password', 400))
+//     }
+
+//     const user = await User.findOne({ email }).select('+password')
+
+//     if (!user || !(await user.validatePassword(password, user.password))) {
+//         return next(new AppError('Incorrect email or password', 401))
+//     }
+// })
--- models/User.js
+++ models/User.js
@@ -1,22 +1,65 @@
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const Sequelize = require('sequelize')
+const validator = require('validator')
 const db = require('../config/database')
-const User = db.define('users', {
-    // password: {
-    //     field: 'user_password',
-    //     type: Sequelize.STRING,
-    //     allowNull: true,
-    // },
-    name: {
-        type: Sequelize.STRING,
-        // field: 'user_name',
-        allowNull: false,
+
+const User = db.define(
+    'users',
+    {
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        passwordConfirm: {
+            type: Sequelize.STRING,
+            allowNull: true,
+            validate: {
+                customValidator(value) {
+                    if (value !== this.password) {
+                        throw new Error('Passwords are not the same')
+                    }
+                },
+            },
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: {
+                    msg: 'Please provide a valid email',
+                },
+            },
+        },
     },
-    email: {
-        type: Sequelize.STRING,
-        // field: 'user_email',
-        allowNull: false,
+    {
+        hooks: {
+            beforeCreate: async (user) => {
+                if (user.password) {
+                    user.password = await bcrypt.hash(user.password, 12)
+                    user.passwordConfirm = undefined
+                }
+            },
+            beforeUpdate: async (user) => {
+                if (user.password) {
+                    user.password = await bcrypt.hash(user.password, 12)
+                    user.passwordConfirm = undefined
+                }
+            },
+        },
+        instanceMethods: {
+            validPassword: (password) => {
+                return bcrypt.compareSync(password, this.password)
+            },
+        },
     },
-})
+)
+
+User.prototype.validPassword = async (password, hash) => {
+    return await bcrypt.compareSync(password, hash)
+}
 
 module.exports = User
